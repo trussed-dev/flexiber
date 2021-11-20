e146cddcdddcf2dba73e8c037fa6c507431b2ebf
nickray <nickray@users.noreply.github.com>
2021-11-20 22:06:20 +0000

    deploy: 825eba57df47a8d084e185eca58912caf231df31

--- implementors/core/convert/trait.AsRef.js
+++ implementors/core/convert/trait.AsRef.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["flexiber"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;[u8]&gt; for <a class=\"struct\" href=\"flexiber/struct.Slice.html\" title=\"struct flexiber::Slice\">Slice</a>&lt;'_&gt;","synthetic":false,"types":["flexiber::slice::Slice"]}];
+implementors["flexiber"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.56.1/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;[u8]&gt; for <a class=\"struct\" href=\"flexiber/struct.Slice.html\" title=\"struct flexiber::Slice\">Slice</a>&lt;'_&gt;","synthetic":false,"types":["flexiber::slice::Slice"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
