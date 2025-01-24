ac5e5c012282f61a2e11d27ea6cec130568a8297
sosthene-nitrokey <sosthene-nitrokey@users.noreply.github.com>
2025-01-24 08:59:28 +0000

    deploy: ded01cb9be2e20a0f29e0cf3263c7b9fca482468

--- trait.impl/core/convert/trait.AsRef.js
+++ trait.impl/core/convert/trait.AsRef.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["flexiber",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;[<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.83.0/core/primitive.u8.html\">u8</a>]&gt; for <a class=\"struct\" href=\"flexiber/struct.Slice.html\" title=\"struct flexiber::Slice\">Slice</a>&lt;'_&gt;"]]]]);
+    var implementors = Object.fromEntries([["flexiber",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;[<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.84.0/core/primitive.u8.html\">u8</a>]&gt; for <a class=\"struct\" href=\"flexiber/struct.Slice.html\" title=\"struct flexiber::Slice\">Slice</a>&lt;'_&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
